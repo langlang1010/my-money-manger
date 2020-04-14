844b27c0403ec57b231e01d62ae085eff8c6e3cd
langlang1010 <3225910491@qq.com>
2020-04-14 16:38:52 +0800

    '优化了界面，通过路由确定active'

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
   <div>
-    <router-view/>
+    <router-view />
     <b-nav justified class="fixed-bottom">
-      <b-nav-item @click="change_active(1)">
-        <template v-if="active === 1">
+      <b-nav-item @click="change_active(0)">
+        <template v-if="active === 0">
           <b-icon icon="plus-circle-fill"></b-icon>
         </template>
         <template v-else> <b-icon icon="plus-circle"></b-icon></template>
-        <br/><small>记账</small>
+        <br /><small>记账</small>
       </b-nav-item>
-      <b-nav-item @click="change_active(2)">
-        <template v-if="active === 2">
+      <b-nav-item @click="change_active(1)">
+        <template v-if="active === 1">
           <b-icon icon="tag-fill"></b-icon>
         </template>
         <template v-else> <b-icon icon="tag"></b-icon> </template>
-        <br/><small>标签</small>
+        <br /><small>标签</small>
       </b-nav-item>
-      <b-nav-item @click="change_active(3)">
-        <template v-if="active === 3">
+      <b-nav-item @click="change_active(2)">
+        <template v-if="active === 2">
           <b-icon icon="bar-chart-fill"></b-icon>
         </template>
         <template v-else> <b-icon icon="bar-chart"></b-icon> </template>
-        <br/><small>统计</small>
+        <br /><small>统计</small>
       </b-nav-item>
     </b-nav>
   </div>
@@ -33,16 +33,23 @@ export default {
   components: {},
   data() {
     return {
-      active: 1,
-      all_routers: ['/','/tag','stastic']
+      active: 0,
+      all_routers: ['/', '/tag', '/stastic']
     }
   },
   methods: {
+    // 切换active
     change_active(which) {
       this.active = which
       // 跳转到相应的page
-       this.$router.push({ path: this.all_routers[which-1] })
+      this.$router.push({ path: this.all_routers[which] })
     }
+  },
+  mounted() {
+    // 根据当前的路由来确定active
+    const name = this.$router.currentRoute.name.toLowerCase()
+    var routerName =  ['record', 'tag', 'stastic']
+    this.active = routerName.indexOf(name)
   }
 }
 </script>
--- src/pages/Record.vue
+++ src/pages/Record.vue
@@ -1,37 +1,135 @@
 <template>
   <div id="record">
     <div>
-      <b-form-tags input-id="tags-basic" v-model="tags" class="mb-2" size="lg"></b-form-tags>
+      <b-form-tags
+        input-id="tags-basic"
+        v-model="tags"
+        class="mb-2"
+        size="lg"
+      ></b-form-tags>
       <!-- <p>Value: {{ value }}</p> -->
     </div>
     <div>
       <b-card no-body>
-        <b-tabs pills card end justified>
-          <b-tab title="收入" active></b-tab>
-          <b-tab title="支出"></b-tab>
+        <b-tabs pills card justified>
+          <b-tab title="支出" active>
+            <div>
+              <b-card class="input-num">
+                <b-card-text>
+                  <b-badge style="float:left" variant="light">支出</b-badge>
+                  {{ input_num }}</b-card-text
+                >
+              </b-card>
+            </div>
+          </b-tab>
+          <b-tab title="收入">
+            <div>
+              <b-card class="input-num">
+                <b-card-text>
+                  <b-badge style="float:left" variant="light">收入</b-badge>
+                  {{ input_num }}</b-card-text
+                >
+              </b-card>
+            </div>
+          </b-tab>
         </b-tabs>
       </b-card>
-      <div>
-        <b-card id="input_num">
-          <b-card-text>{{ input_num }}</b-card-text>
-        </b-card>
-      </div>
-      <div>
-        <b-button @click="input(1)" squared class="num-button" variant="secondary">1</b-button>
-        <b-button @click="input(2)" squared class="num-button" variant="secondary">2</b-button>
-        <b-button @click="input(3)" squared class="num-button" variant="secondary">3</b-button>
-        <b-button @click="del()" squared class="num-button" variant="secondary">删除</b-button>
-        <b-button @click="input(4)" squared class="num-button" variant="secondary">4</b-button>
-        <b-button @click="input(5)" squared class="num-button" variant="secondary">5</b-button>
-        <b-button @click="input(6)" squared class="num-button" variant="secondary">6</b-button>
-        <b-button @click="clear()" squared class="num-button" variant="secondary">清空</b-button>
-        <b-button @click="input(7)" squared class="num-button" variant="secondary">7</b-button>
-        <b-button @click="input(8)" squared class="num-button" variant="secondary">8</b-button>
-        <b-button @click="input(9)" squared class="num-button" variant="secondary">9</b-button>
-        <b-button @click="pushmoney()" squared class="num-button-ok" variant="secondary">OK</b-button>
-        <b-button @click="input(0)" squared class="num-button-zero" variant="secondary">0</b-button>
-        <b-button @click="add_point()" squared class="num-button" variant="secondary">.</b-button>
 
+      <div>
+        <b-button
+          @click="input(1)"
+          squared
+          class="num-button"
+          variant="secondary"
+          >1</b-button
+        >
+        <b-button
+          @click="input(2)"
+          squared
+          class="num-button"
+          variant="secondary"
+          >2</b-button
+        >
+        <b-button
+          @click="input(3)"
+          squared
+          class="num-button"
+          variant="secondary"
+          >3</b-button
+        >
+        <b-button @click="del()" squared class="num-button" variant="secondary"
+          >删除</b-button
+        >
+        <b-button
+          @click="input(4)"
+          squared
+          class="num-button"
+          variant="secondary"
+          >4</b-button
+        >
+        <b-button
+          @click="input(5)"
+          squared
+          class="num-button"
+          variant="secondary"
+          >5</b-button
+        >
+        <b-button
+          @click="input(6)"
+          squared
+          class="num-button"
+          variant="secondary"
+          >6</b-button
+        >
+        <b-button
+          @click="clear()"
+          squared
+          class="num-button"
+          variant="secondary"
+          >清空</b-button
+        >
+        <b-button
+          @click="input(7)"
+          squared
+          class="num-button"
+          variant="secondary"
+          >7</b-button
+        >
+        <b-button
+          @click="input(8)"
+          squared
+          class="num-button"
+          variant="secondary"
+          >8</b-button
+        >
+        <b-button
+          @click="input(9)"
+          squared
+          class="num-button"
+          variant="secondary"
+          >9</b-button
+        >
+        <b-button
+          @click="pushmoney()"
+          squared
+          class="num-button-ok"
+          variant="secondary"
+          >OK</b-button
+        >
+        <b-button
+          @click="input(0)"
+          squared
+          class="num-button-zero"
+          variant="secondary"
+          >0</b-button
+        >
+        <b-button
+          @click="add_point()"
+          squared
+          class="num-button"
+          variant="secondary"
+          >.</b-button
+        >
       </div>
     </div>
     <!-- 显示输入的数字 -->
@@ -42,71 +140,60 @@
 export default {
   data() {
     return {
-      input_num: "0",
-      point:false,
-      tags: ["衣", "食", "住", "行"],
-      income: "",
-      outcome: "",
-      
-    };
+      input_num: '0',
+      point: false,
+      tags: ['衣', '食', '住', '行'],
+      income: '',
+      outcome: ''
+    }
   },
   methods: {
     input(num) {
       // 如果是刚开始输入
-      if (this.input_num === "0") {
-        this.input_num = num + "";
+      if (this.input_num === '0') {
+        this.input_num = num + ''
       } else {
-        this.input_num += num;
+        this.input_num += num
       }
     },
     // 点击小数点时
     add_point() {
       // 如果没有点过小数点
-       
-       if (!this.point) {
-          this.point=true;
-          this.input_num += ".";
-         
-       }
-      
-    
-    
+
+      if (!this.point) {
+        this.point = true
+        this.input_num += '.'
+      }
     },
-    
+
     // 清空
     clear() {
-      this.input_num = "0";
-      this.point=false;
+      this.input_num = '0'
+      this.point = false
     },
     // 回删
     del() {
-       var length=this.input_num.length;
-         if(this.input_num.charAt(length-1)==="."){
-        this.point=false;
-          this.input_num = this.input_num.substr(0, this.input_num.length - 1);
-      }else{
-        this.input_num = this.input_num.substr(0, this.input_num.length - 1);
+      var length = this.input_num.length
+      if (this.input_num.charAt(length - 1) === '.') {
+        this.point = false
       }
-    
-     
-    
-   
+      this.input_num = this.input_num.substr(0, this.input_num.length - 1)
     },
     pushmoney() {
-      var pointlast = this.input_num.indexOf(".");
-      var length = this.input_num.length;
-      var result = this.input_num.substr(pointlast + 1, length);
-      var plength = result.length;
+      var pointlast = this.input_num.indexOf('.')
+      var length = this.input_num.length
+      var result = this.input_num.substr(pointlast + 1, length)
+      var plength = result.length
       if (plength > 2 && pointlast !== -1) {
-        alert("输入金额不对");
-        this.input_num = "0";
+        alert('输入金额不对')
+        this.input_num = '0'
       } else {
-        this.income = parseFloat(this.input_num);
-        alert(this.income);
+        this.income = parseFloat(this.input_num)
+        alert(this.income)
       }
     }
   }
-};
+}
 </script>
 
 <style>
@@ -132,7 +219,7 @@ export default {
   height: 128px;
   float: right;
 }
-#input_num {
+.input-num {
   text-align: right;
 }
 </style>
