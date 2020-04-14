aa97ab6a0f67130e4d5886f94b09ccc9a8bb6e4c
langlang1010 <3225910491@qq.com>
2020-04-14 13:24:06 +0800

    提交数字

--- src/pages/Record.vue
+++ src/pages/Record.vue
@@ -6,59 +6,33 @@
     </div>
     <div>
       <b-card no-body>
-        <b-tabs card justified>
-          <b-tab title="收入" active>
-            <b-card-text>
-              <div>
-                <b-card id="input_num">
-                  <b-card-text>{{ input_num }}</b-card-text>
-                </b-card>
-              </div>
-              <div>
-                <b-button @click="input(1)" squared class="num-button" variant="secondary">1</b-button>
-                <b-button @click="input(2)" squared class="num-button" variant="secondary">2</b-button>
-                <b-button @click="input(3)" squared class="num-button" variant="secondary">3</b-button>
-                <b-button @click="del()" squared class="num-button" variant="secondary">删除</b-button>
-                <b-button @click="input(4)" squared class="num-button" variant="secondary">4</b-button>
-                <b-button @click="input(5)" squared class="num-button" variant="secondary">5</b-button>
-                <b-button @click="input(6)" squared class="num-button" variant="secondary">6</b-button>
-                <b-button @click="clear()" squared class="num-button" variant="secondary">清空</b-button>
-                <b-button @click="input(7)" squared class="num-button" variant="secondary">7</b-button>
-                <b-button @click="input(8)" squared class="num-button" variant="secondary">8</b-button>
-                <b-button @click="input(9)" squared class="num-button" variant="secondary">9</b-button>
-                <b-button squared class="num-button-ok" variant="secondary">OK</b-button>
-                <b-button @click="input(0)" squared class="num-button-zero" variant="secondary">0</b-button>
-                <b-button @click="add_point" squared class="num-button" variant="secondary">.</b-button>
-              </div>
-            </b-card-text>
-          </b-tab>
-          <b-tab title="支出">
-            <b-card-text>
-              <div>
-                <b-card id="input_num">
-                  <b-card-text>{{ input_num }}</b-card-text>
-                </b-card>
-              </div>
-              <div>
-                <b-button @click="input(1)" squared class="num-button" variant="secondary">1</b-button>
-                <b-button @click="input(2)" squared class="num-button" variant="secondary">2</b-button>
-                <b-button @click="input(3)" squared class="num-button" variant="secondary">3</b-button>
-                <b-button @click="del()" squared class="num-button" variant="secondary">删除</b-button>
-                <b-button @click="input(4)" squared class="num-button" variant="secondary">4</b-button>
-                <b-button @click="input(5)" squared class="num-button" variant="secondary">5</b-button>
-                <b-button @click="input(6)" squared class="num-button" variant="secondary">6</b-button>
-                <b-button @click="clear()" squared class="num-button" variant="secondary">清空</b-button>
-                <b-button @click="input(7)" squared class="num-button" variant="secondary">7</b-button>
-                <b-button @click="input(8)" squared class="num-button" variant="secondary">8</b-button>
-                <b-button @click="input(9)" squared class="num-button" variant="secondary">9</b-button>
-                <b-button squared class="num-button-ok" variant="secondary">OK</b-button>
-                <b-button @click="input(0)" squared class="num-button-zero" variant="secondary">0</b-button>
-                <b-button @click="add_point" squared class="num-button" variant="secondary">.</b-button>
-              </div>
-            </b-card-text>
-          </b-tab>
+        <b-tabs pills card end justified>
+          <b-tab title="收入" active></b-tab>
+          <b-tab title="支出"></b-tab>
         </b-tabs>
       </b-card>
+      <div>
+        <b-card id="input_num">
+          <b-card-text>{{ input_num }}</b-card-text>
+        </b-card>
+      </div>
+      <div>
+        <b-button @click="input(1)" squared class="num-button" variant="secondary">1</b-button>
+        <b-button @click="input(2)" squared class="num-button" variant="secondary">2</b-button>
+        <b-button @click="input(3)" squared class="num-button" variant="secondary">3</b-button>
+        <b-button @click="del()" squared class="num-button" variant="secondary">删除</b-button>
+        <b-button @click="input(4)" squared class="num-button" variant="secondary">4</b-button>
+        <b-button @click="input(5)" squared class="num-button" variant="secondary">5</b-button>
+        <b-button @click="input(6)" squared class="num-button" variant="secondary">6</b-button>
+        <b-button @click="clear()" squared class="num-button" variant="secondary">清空</b-button>
+        <b-button @click="input(7)" squared class="num-button" variant="secondary">7</b-button>
+        <b-button @click="input(8)" squared class="num-button" variant="secondary">8</b-button>
+        <b-button @click="input(9)" squared class="num-button" variant="secondary">9</b-button>
+        <b-button @click="pushmoney()" squared class="num-button-ok" variant="secondary">OK</b-button>
+        <b-button @click="input(0)" squared class="num-button-zero" variant="secondary">0</b-button>
+        <b-button @click="add_point()" squared class="num-button" variant="secondary">.</b-button>
+
+      </div>
     </div>
     <!-- 显示输入的数字 -->
   </div>
@@ -69,8 +43,11 @@ export default {
   data() {
     return {
       input_num: "0",
-      point: false,
-      tags: ["衣", "食", "住", "行"]
+      point:false,
+      tags: ["衣", "食", "住", "行"],
+      income: "",
+      outcome: "",
+      
     };
   },
   methods: {
@@ -85,18 +62,48 @@ export default {
     // 点击小数点时
     add_point() {
       // 如果没有点过小数点
-      if (!this.point) {
-        this.point = true;
-        this.input_num += ".";
-      }
+       
+       if (!this.point) {
+          this.point=true;
+          this.input_num += ".";
+         
+       }
+      
+    
+    
     },
+    
     // 清空
     clear() {
       this.input_num = "0";
+      this.point=false;
     },
     // 回删
     del() {
-      this.input_num = this.input_num.substr(0, this.input_num.length - 1);
+       var length=this.input_num.length;
+         if(this.input_num.charAt(length-1)==="."){
+        this.point=false;
+          this.input_num = this.input_num.substr(0, this.input_num.length - 1);
+      }else{
+        this.input_num = this.input_num.substr(0, this.input_num.length - 1);
+      }
+    
+     
+    
+   
+    },
+    pushmoney() {
+      var pointlast = this.input_num.indexOf(".");
+      var length = this.input_num.length;
+      var result = this.input_num.substr(pointlast + 1, length);
+      var plength = result.length;
+      if (plength > 2 && pointlast !== -1) {
+        alert("输入金额不对");
+        this.input_num = "0";
+      } else {
+        this.income = parseFloat(this.input_num);
+        alert(this.income);
+      }
     }
   }
 };
